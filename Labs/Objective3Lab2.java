75fa3a6129e6f1f320d2de8cc1c59427de574820
Thomas Webel <Thomaswebel@gmail.com>
2021-01-05 15:07:24 -0500

    Complete Obj3Labs1-3

---
+++ Labs/Objective3Lab2.java
@@ -0,0 +1,7 @@
+public class Objective3Lab2 {
+  public static void main(String[] args) {
+    double side1 = 10, side2 = 8;
+    double hypotenuse = Math.sqrt((Math.pow(side1, 2) + Math.pow(side2, 2)));
+    System.out.println(hypotenuse);
+  }
+}
